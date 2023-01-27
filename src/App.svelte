a92f36c422825dbc5cb2b953011007945fb8f99d
jerico2992 <80119275+jerico2992@users.noreply.github.com>
2023-01-26 23:00:03 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,260 @@
+<script>
+  import PlayerPanel from "./PlayerPanel.svelte";
+  let players=[
+    {
+        name: "Blue",
+        team: 0,
+        boost: 33,
+        score: 0,
+    },
+    {
+        name: "Orange",
+        team: 1,
+        boost: 33,
+    }
+  ];
+  let teams = [
+    {score: 0},
+    {score: 0},
+  ];
+  let time=300;
+  let OT=false;
+  let focus;
+  let t1Wins=0;
+  let t2Wins=0;
+  let stats = false;
+  $: seconds = time % 60;
+  $: minutes = Math.floor(time/60);
+  const WsSubscribers = {
+    __subscribers: {},
+    websocket: undefined,
+    webSocketConnected: false,
+    registerQueue: [],
+    init: function(port, debug, debugFilters) {
+        port = port || 49322;
+        debug = debug || false;
+        if (debug) {
+            if (debugFilters !== undefined) {
+                console.warn("WebSocket Debug Mode enabled with filtering. Only events not in the filter list will be dumped");
+            } else {
+                console.warn("WebSocket Debug Mode enabled without filters applied. All events will be dumped to console");
+                console.warn("To use filters, pass in an array of 'channel:event' strings to the second parameter of the init function");
+            }
+        }
+        WsSubscribers.webSocket = new WebSocket("ws://localhost:" + port);
+        WsSubscribers.webSocket.onmessage = function (event) {
+            let jEvent = JSON.parse(event.data);
+            if (!jEvent.hasOwnProperty('event')) {
+                return;
+            }
+            let eventSplit = jEvent.event.split(':');
+            let channel = eventSplit[0];
+            let event_event = eventSplit[1];
+            if (debug) {
+                if (!debugFilters) {
+                    console.log(channel, event_event, jEvent);
+                } else if (debugFilters && debugFilters.indexOf(jEvent.event) < 0) {
+                    console.log(channel, event_event, jEvent);
+                }
+            }
+            WsSubscribers.triggerSubscribers(channel, event_event, jEvent.data);
+        };
+        WsSubscribers.webSocket.onopen = function () {
+            WsSubscribers.triggerSubscribers("ws", "open");
+            WsSubscribers.webSocketConnected = true;
+            WsSubscribers.registerQueue.forEach((r) => {
+                WsSubscribers.send("wsRelay", "register", r);
+            });
+            WsSubscribers.registerQueue = [];
+        };
+        WsSubscribers.webSocket.onerror = function () {
+            WsSubscribers.triggerSubscribers("ws", "error");
+            WsSubscribers.webSocketConnected = false;
+        };
+        WsSubscribers.webSocket.onclose = function () {
+            WsSubscribers.triggerSubscribers("ws", "close");
+            WsSubscribers.webSocketConnected = false;
+        };
+    },
+    /**
+     * Add callbacks for when certain events are thrown
+     * Execution is guaranteed to be in First In First Out order
+     * @param channels
+     * @param events
+     * @param callback
+     */
+    subscribe: function(channels, events, callback) {
+        if (typeof channels === "string") {
+            let channel = channels;
+            channels = [];
+            channels.push(channel);
+        }
+        if (typeof events === "string") {
+            let event = events;
+            events = [];
+            events.push(event);
+        }
+        channels.forEach(function(c) {
+            events.forEach(function (e) {
+                if (!WsSubscribers.__subscribers.hasOwnProperty(c)) {
+                    WsSubscribers.__subscribers[c] = {};
+                }
+                if (!WsSubscribers.__subscribers[c].hasOwnProperty(e)) {
+                    WsSubscribers.__subscribers[c][e] = [];
+                    if (WsSubscribers.webSocketConnected) {
+                        WsSubscribers.send("wsRelay", "register", `${c}:${e}`);
+                    } else {
+                        WsSubscribers.registerQueue.push(`${c}:${e}`);
+                    }
+                }
+                WsSubscribers.__subscribers[c][e].push(callback);
+            });
+        })
+    },
+    clearEventCallbacks: function (channel, event) {
+        if (WsSubscribers.__subscribers.hasOwnProperty(channel) && WsSubscribers.__subscribers[channel].hasOwnProperty(event)) {
+            WsSubscribers.__subscribers[channel] = {};
+        }
+    },
+    triggerSubscribers: function (channel, event, data) {
+        if (WsSubscribers.__subscribers.hasOwnProperty(channel) && WsSubscribers.__subscribers[channel].hasOwnProperty(event)) {
+            WsSubscribers.__subscribers[channel][event].forEach(function(callback) {
+                if (callback instanceof Function) {
+                    callback(data);
+                }
+            });
+        }
+    },
+    send: function (channel, event, data) {
+        if (typeof channel !== 'string') {
+            console.error("Channel must be a string");
+            return;
+        }
+        if (typeof event !== 'string') {
+            console.error("Event must be a string");
+            return;
+        }
+        if (channel === 'local') {
+            this.triggerSubscribers(channel, event, data);
+        } else {
+            let cEvent = channel + ":" + event;
+            WsSubscribers.webSocket.send(JSON.stringify({
+                'event': cEvent,
+                'data': data
+            }));
+        }
+    }
+  };
+  WsSubscribers.init(49322, false);
+  WsSubscribers.subscribe('game', 'update_state', (d)=>{
+    const num = Object.values(d.players).length;
+    if(num>1 && num<3) {
+        players=Object.values(d.players); 
+    }  
+    teams=d.game.teams;
+    time=d.game.time_seconds;
+    OT=d.game.isOT;
+    focus=d.game.target;
+});
+WsSubscribers.subscribe('game', 'match_ended', (d)=>{
+    console.log(d);
+    if(d.winner_team_num===0) {
+        t1Wins = t1Wins+1;
+    } else {
+        t2Wins = t2Wins+1;
+    }
+    stats = true;
+});
+
+WsSubscribers.subscribe('game', 'match_destroyed', (d)=>{
+    stats = false;
+});
+</script>
+
+<main>
+    {#if players[0].team===0}
+     <PlayerPanel bind:player={players[0]} bind:wins={t1Wins} bind:goals={teams[0].score}></PlayerPanel>
+     <div class='clock'>
+        {#if !OT}
+            {#if seconds<10}
+               <h1>{minutes}:0{seconds}</h1>
+            {:else}
+               <h1>{minutes}:{seconds}</h1>
+            {/if}
+        {:else}
+            {#if seconds<10}
+              <h1>+{minutes}:0{seconds}</h1>
+           {:else}
+              <h1>+{minutes}:{seconds}</h1>
+           {/if}
+        {/if}
+    </div>
+     <PlayerPanel bind:player={players[1]} bind:wins={t2Wins} bind:goals={teams[1].score}></PlayerPanel>
+    {:else}
+     <PlayerPanel bind:player={players[1]} bind:wins={t1Wins} bind:goals={teams[0].score}></PlayerPanel>
+     <div class='clock'>
+        {#if !OT}
+            {#if seconds<10}
+               <h1>{minutes}:0{seconds}</h1>
+            {:else}
+               <h1>{minutes}:{seconds}</h1>
+            {/if}
+        {:else}
+            {#if seconds<10}
+              <h1>+{minutes}:0{seconds}</h1>
+           {:else}
+              <h1>+{minutes}:{seconds}</h1>
+           {/if}
+        {/if}
+    </div>
+     <PlayerPanel bind:player={players[0]} bind:wins={t2Wins} bind:goals={teams[1].score}></PlayerPanel>
+    {/if}
+    {#if stats}
+        <div class='stats-contain'>
+            <table>
+                <tr><th></th><th>{players[0].name}</th><th>{players[1].name}</th></tr>
+                <tr><td>Score</td><td>{players[0].score}</td><td>{players[1].score}</td></tr>
+                <tr><td>Shots</td><td>{players[0].shots}</td><td>{players[1].shots}</td></tr>
+                <tr><td>Saves</td><td>{players[0].saves}</td><td>{players[1].saves}</td></tr>
+                <tr><td>Demos</td><td>{players[0].demos}</td><td>{players[1].demos}</td></tr>
+            </table>
+        </div>
+    {/if}
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    align-items: flex-start;
+    justify-content: center;
+  }
+  .clock {
+    background-color: black;
+    color: white;
+  }
+  h1{
+        margin: 0;
+        font-size: 2.5rem;
+        padding: 0.35em;
+    }
+    .stats-contain {
+        position: fixed;
+        left: 50%;
+        transform: translate(-50%, 0%);
+        bottom: 20%;
+    }
+    table, th, td {
+        font-size: 1.5em;
+        padding: 0.7em;
+    }
+    table {
+        background-image: linear-gradient(-45deg, rgb(150, 150, 150), rgb(50, 50, 50));
+        border-collapse: collapse;
+        border-radius: 0.5em;
+    }
+    th {
+        padding: 0.7em;
+    }
+</style>
---
+++ src/PlayerPanel.svelte
@@ -0,0 +1,108 @@
+<script>
+    export let player;
+    export let wins=0;
+    export let goals=0;
+    import { tweened } from "svelte/motion";
+    const bTween = tweened(player.boost,
+    {
+        duration: 100,
+    });
+    $: bTween.set(player.boost);
+</script>
+<div class="main">
+    {#if player.team===1}
+        <div class="score-2">
+            <h1>{goals}</h1>
+            <div class="win-ticks">
+                {#each Array(wins) as _}
+                    <div style="background: rgb(10, 10, 10); min-height: 5px; min-width: 5px; max-width: 5px; margin-left: 5px;"></div>
+                {/each}
+            </div>
+        </div>
+    {/if}
+    <div class="name-plate">
+        {#if player.team===0}
+            <div class="p1-boost">
+                <div style="background: white; min-height: 0.5rem; min-width: {$bTween/100*22}vw"></div>               
+            </div>
+            <div class="p1-name-div">
+                <h1>{player.name}</h1>
+            </div>
+        {:else}
+            <div class="p2-boost">
+                <div style="background: white; min-height: 0.5rem; min-width: {$bTween/100*22}vw"></div>
+            </div>
+            <div class="p2-name-div">            
+                <h1>{player.name}</h1>
+            </div>
+        {/if}
+    </div>
+    {#if player.team===0}
+        <div class="score-1">
+            <h1>{goals}</h1>
+            <div class="win-ticks">
+                {#each Array(wins) as _}
+                    <div style="background: rgb(10, 10, 10); min-height: 5px; min-width: 5px; max-width: 5px; margin-left: 5px;"></div>
+                {/each}
+            </div>
+        </div>
+    {/if}
+</div>
+<style>
+    .main {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        margin:0;
+        
+    }
+    .name-plate {
+        width: 22vw;
+        min-width: 22vw;
+    }
+    h1{
+        margin: 0;
+        font-size: 2.5rem;
+        padding: 0.35em;
+    }
+    .p1-name-div {
+        color: white;
+        background-image: linear-gradient(-45deg, rgb(0, 0, 255), rgb(0, 0, 100));
+    }
+    .p2-name-div {
+        color: white;
+        background-image: linear-gradient(-45deg, rgb(210, 140, 0), rgb(111, 54, 0));
+    }
+    .p1-boost {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-end;
+        min-width: 22vw;
+        min-height: 0.5rem;
+        background-color: rgb(40, 40, 40);
+    }
+    .p2-boost {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        min-width: 22vw;
+        min-height: 0.5rem;
+        background-color: rgb(40, 40, 40);
+    }
+    .score-1 {
+        display: flex;
+        flex-direction: column;
+        flex-wrap: nowrap;
+        background-image: linear-gradient(rgb(0, 100, 255), rgb(0, 200, 255));
+    }
+    .score-2 {
+        display: flex;
+        flex-direction: column;
+        flex-wrap: nowrap;
+        background-image: linear-gradient(rgb(180, 120, 0), rgb(255, 220, 0))
+    }
+    .win-ticks {
+        display: flex;
+        flex-direction: row;
+    }
+</style>
