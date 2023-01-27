eaa2543df8ef02ed36365597f6c8d4c318f24e75
jerico2992 <80119275+jerico2992@users.noreply.github.com>
2023-01-26 23:08:15 -0500

    change port to 8080

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    port:8080,
+    strictPort:false,
+  },
+  preview: {
+    port:8080,
+    strictPort: false,
+  }
 })
